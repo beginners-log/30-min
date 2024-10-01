da9fb77627bd27c1d666bff52d65a20c62f77446
beginners-log <oxbaxmann@gmail.com>
2024-10-01 17:10:17 +0300

    JQ 6

--- cypress/e2e/1-getting-started/configuration.cy.js
+++ cypress/e2e/1-getting-started/configuration.cy.js
@@ -5,10 +5,20 @@ describe.only('Verify configuration', () => {
     console.log(Cypress.env())
   })
 
-  it.only('checks the respective environment is applied', () => {
+  it.only('1', () => {
     const user = Cypress.env().user_1
+
+    if (typeof user.password !== 'string' || !'111') {
+      throw new Error('Missing password value, set using CYPRESS_password=...')
+    }
 
     expect(Cypress.config().baseUrl).to.include('/dev')
     expect(user.password).to.eq('111', { log: false })
+  })
+
+  it.only('2', () => {
+    const user = Cypress.env().user_1
+
+    expect(user.password === '111').to.be.true
   })
 })
